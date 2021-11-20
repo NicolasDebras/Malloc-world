b89d67537edf2211b26f96f25775e2f0d2857b68
Nicolas Debras <nicodebras17@gmail.com>
2021-11-20 14:29:01 +0100

    add different menu

--- nicolas/png.c
+++ nicolas/png.c
@@ -28,14 +28,63 @@ void read_print_png() {
 // fonction qui affiche les diff info et option dispo avec le png
 void print_info() {
 
-    printf("votre choix : \n-1 :  \n-2 \n-3\n");
+    printf("votre choix : \n");
+    printf("1 : CRAFTER ELEMENTS\n");
+    printf("2 : REPARATION\n");
+    printf("3 : VOTRE BANQUE\n");
+    printf("4 : QUITTER\n");
+    printf("\n\n\n");
+    printf("Veuillez saisir le numéro de votre choix ");
 
+}
+// a completer avec toutes les differents uppgrade possible
+void print_menu_up_tool() {
+
+    printf("votre choix : \n");
+    printf("1 : CRAFTER ELEMENTS\n");
+    printf("2 : REPARATION\n");
+    printf("3 : VOTRE BANQUE\n");
+    printf("4 : QUITTER\n");
+    printf("\n\n\n");
+    printf("Veuillez saisir le numéro de votre choix ");
+    int c = atoi(input_char());
+    // fonction de craft @soulte92
+
+}
+
+int selected_choose(int c)
+{
+
+    if (c == 1) {
+        print_menu_up_tool();
+        return 0;
+
+    }
+    else if (c == 2) {
+        // fonction de reparation @soulte92
+        return 0;
+    }
+    else if (c == 3) {
+        //banque @soulte92 
+        return 0;
+    }
+    else if (c == 4) {
+        // banque @soulte92
+        return 1;
+    }
+    else
+        return 42;
 }
 
 //fonction principal
 void png_interaction() {
 
     read_print_png();
-    print_info();
+    while(1) {
+        print_info();
+        int c = atoi(input_char());
+        if (selected_choose(c) == 1)
+            break;
+    }
     
 }
