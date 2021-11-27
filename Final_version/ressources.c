5d87673cd72a8a0c7a607532ed93cf7cf040c9ee
Souleman <soulte92@gmail.com>
2021-11-27 15:06:41 +0100

    adding new collecte fontion

--- Final_version/ressources.c
+++ Final_version/ressources.c
@@ -86,14 +86,17 @@ ressource_collect *collect_ressources(int ressources, map *m, int pos_x, int pos
 
     if (m->level == 1) {
         new = add_element(old, nb, ressources, m->level, pos_x, pos_y, turn);
+        // collectRessourceAndToolUpdate(p->inventory,map_id_to_inv_id(ressources),RangedRand(3,5),mapZone);
         collectCrafts(p->inventory, RangedRand(3, 5),map_id_to_inv_id(ressources));
         //fonction qui ajoute des ressources dans l'inventaire @soulte92
     } else if (m->level == 2 ) {
         new = add_element(old, nb, ressources, m->level, pos_x, pos_y, turn);
+        // collectRessourceAndToolUpdate(p->inventory,map_id_to_inv_id(ressources),RangedRand(3,5),mapZone);
         collectCrafts(p->inventory, RangedRand(3, 5),map_id_to_inv_id(ressources));
         //fonction qui ajoute des ressources dans l'inventaire  @soulte92
     } else {
         new = add_element(old, nb, ressources, m->level, pos_x, pos_y, turn);
+        // collectRessourceAndToolUpdate(p->inventory,map_id_to_inv_id(ressources),RangedRand(3,5),mapZone);
         collectCrafts(p->inventory, RangedRand(3, 5),map_id_to_inv_id(ressources));
         //fonction qui ajoute des ressources dans l'inventaire @soulte92
     }
